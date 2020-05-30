b9da26c89853f90903463bd5774e8ad0d27e8e84
unknown <ChrisVou@hotmail.com>
2020-05-30 20:15:21 +0300

    Updates

---
+++ precache-manifest.34c3884a39b2e9be4b1d25ff3f352072.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36dfe24cf75437164b8160a859267160",
+    "url": "/monsters-app/index.html"
+  },
+  {
+    "revision": "7f6c88090e299d87ca09",
+    "url": "/monsters-app/static/css/main.4c7a091b.chunk.css"
+  },
+  {
+    "revision": "76e1e33d0dc86b7a1eb3",
+    "url": "/monsters-app/static/js/2.2e94f083.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-app/static/js/2.2e94f083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f6c88090e299d87ca09",
+    "url": "/monsters-app/static/js/main.20adea1e.chunk.js"
+  },
+  {
+    "revision": "9cd392af60b73ea37e8f",
+    "url": "/monsters-app/static/js/runtime-main.ce4bea50.js"
+  }
+]);
